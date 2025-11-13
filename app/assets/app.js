23d0045f214a9a794cf784fb1cacdbc774ba4656
Piotr Ramotowski <piotr@ramotowski.com>
2025-11-13 17:29:22 +0000

    Add assets

---
+++ app/assets/app.js
@@ -0,0 +1,8 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import * as bootstrap from 'bootstrap';
+
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css';
+
+import './styles/app.css';
+
+window.bootstrap = bootstrap;
